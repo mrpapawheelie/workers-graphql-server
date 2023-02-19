d2b71581b8452a48ab18edfa2d549f2894a7b087
PapaWheelie <86974690+mrpapawheelie@users.noreply.github.com>
2023-02-19 13:55:23 -0700

    coingecko

--- src/schema.js
+++ src/schema.js
@@ -1,26 +1,44 @@
-const { gql } = require('apollo-server-cloudflare')
+const { gql } = require('apollo-server-cloudflare');
 
 module.exports = gql`
-  type PokemonSprites {
-    front_default: String!
-    front_shiny: String!
-    front_female: String!
-    front_shiny_female: String!
-    back_default: String!
-    back_shiny: String!
-    back_female: String!
-    back_shiny_female: String!
-  }
-
-  type Pokemon {
+  type Coin {
     id: ID!
     name: String!
-    height: Int!
-    weight: Int!
-    sprites: PokemonSprites!
+    symbol: String!
+    price: Float!
+    market_cap: Float!
+    circulating_supply: Float!
+    max_supply: Float!
+    high_24h: Float!
+    low_24h: Float!
+    change_24h: Float!
+  }
+
+  type NFT {
+    id: ID!
+    name: String!
+    symbol: String!
+    price: Float!
+    market_cap: Float!
+    circulating_supply: Float!
+  }
+
+  type Market {
+    id: ID!
+    name: String!
+    symbol: String!
+    price: Float!
+    market_cap: Float!
+    volume_24h: Float!
+    change_24h: Float!
   }
 
   type Query {
-    pokemon(id: ID!): Pokemon
+    coin(id: ID!): Coin
+    nft(id: ID!): NFT
+    market(id: ID!): Market
+    coins: [Coin]
+    nfts: [NFT]
+    markets: [Market]
   }
-`
+`;
